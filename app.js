7b8bd107285fc3190ce3a7ec1a03683acd1f664e
Reda Beloued <reda.exclusive@gmail.com>
2020-09-10 11:42:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var appData =
     [
         { id: "sslpost", name: "SSLP365", service: "SSLPost", endpoint: "https://secure.sslpost.com/app/xml/encrypt/" },
         { id: "mailadoc", name: "MailaDoc365", service: "MailaDoc", endpoint: "https://portal.mailadoc.co.uk/app/xml/encrypt/" },
-        { id: "securedd", name: "SecureDD365", service: "SecureDD", endpoint: "https://secure.sslposteurope.com/" }
+        { id: "securedd", name: "SecureDD365", service: "SecureDD", endpoint: "https://secure.sslposteurope.com/app/xml/encrypt/" }
     ];
-//sslpost
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const appid = urlParams.get('appid');
 
-    appInfo= appData.find(ap => ap.id === appid);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const appid = urlParams.get('appid');
+
+appInfo= appData.find(ap => ap.id === appid);
