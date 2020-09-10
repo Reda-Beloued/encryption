e3c1892784caf6884544fe2070a37b9d87afc7e5
Reda Beloued <reda.exclusive@gmail.com>
2020-09-10 11:38:54 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var appData =
     [
         { id: "sslpost", name: "SSLP365", service: "SSLPost", endpoint: "https://secure.sslpost.com/app/xml/encrypt/" },
-
-        { id: "mailadoc", name: "MailaDoc365", service: "MailaDoc", endpoint: "https://portal.mailadoc.co.uk/app/xml/encrypt/" }
+        { id: "mailadoc", name: "MailaDoc365", service: "MailaDoc", endpoint: "https://portal.mailadoc.co.uk/app/xml/encrypt/" },
+        { id: "securedd", name: "SecureDD365", service: "SecureDD", endpoint: "https://secure.sslposteurope.com/" }
     ];
 //sslpost
     const queryString = window.location.search;
