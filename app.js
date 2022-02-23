4c8e5a386f73ee77a8de5601f9bc07f2b8a6f303
Reda Beloued <reda.exclusive@gmail.com>
2022-02-23 17:52:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var appData =
             accountUrl: "https://secure.sslposteurope.com/app/"
         },
         {
-            id: "sslpost-dev", name: "SSLP Dev365", service: "SSLP Dev",
+            id: "sslpdev", name: "SSLP Dev365", service: "SSLP Dev",
             endpoint: "https://jollynoyce.dev.sslpost.com/app/xml/encrypt/",
             configUrl: null,
             accountUrl:"https://jollynoyce.dev.sslpost.com/app/"
